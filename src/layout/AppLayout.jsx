bba89b866cb507e690ad0d2639cb6b05ef3cc5f9
JongryunKo <secwind1206@gmail.com>
2024-08-30 16:13:54 +0900

    first commit

---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,47 @@
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Outlet } from 'react-router-dom';
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Navbar expand="lg" className="" style={{backgroundColor:'black'}}>
+        <Container fluid>
+          <Navbar.Brand href="/"><img style={{width:200, height:60}} src='https://images.ctfassets.net/4cd45et68cgf/4nBnsuPq03diC5eHXnQYx/d48a4664cdc48b6065b0be2d0c7bc388/Netflix-Logo.jpg'></img></Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="/" style={{color:'white'}}>Home</Nav.Link>
+              <Nav.Link href="/movies" style={{color:'white'}}>Movies</Nav.Link>
+
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="search">
+                <FontAwesomeIcon icon={faMagnifyingGlass} style={{color:"red"}} />
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
+  )
+}
+
+export default AppLayout
